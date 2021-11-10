f17ab249fa13b321f6ec2e53bb3933e668a91d4a
Peterson <petersonzeferino@hotmail.com>
2021-11-10 16:30:47 -0300

    Create onError function

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
+
 console.log('Started API in port' + port);
 
 function normalizePort(val){
@@ -33,4 +35,26 @@ function normalizePort(val){
         return port;
 
     return false;
+}
+
+function onError(error){
+    if(error.syscall != 'listen')
+        throw error
+    
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code){
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    };
 }

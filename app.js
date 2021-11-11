a9ff59d24ddcb051edc6e92e3465b59e750f822d
Peterson <petersonzeferino@hotmail.com>
2021-11-11 10:13:54 -0300

    Remove parse and Cors

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const index = require('./routes/index-route');
 
 app.use('/', index);
 
-app.use(bodyParser.json({
-    limit: '5mb'
-}));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// app.use(bodyParser.json({
+//     limit: '5mb'
+// }));
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+// });
 
 module.exports = app;
